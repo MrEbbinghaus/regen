c241b5032987d29e43584c65711ae877a9dfb4a4
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2021-12-08 13:08:07 +0100

    Update build process

--- build.clj
+++ build.clj
@@ -1,27 +1,36 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
-            [org.corfield.build :as bb]))
+  (:require 
+   [org.corfield.build :as bb]))
 
 (def lib 'me.ebbinghaus/regen)
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version "1.0.0")
+
+(defn assoc-lib-info [opts]
+  (assoc opts :lib lib :version version :src-pom "template/pom.xml"))
 
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
   (-> opts
-      (assoc :lib lib :version version)
+      assoc-lib-info
       (bb/run-tests)
       (bb/clean)
       (bb/jar)))
 
 (defn install "Install the JAR locally." [opts]
   (-> opts
-      (assoc :lib lib :version version)
+      assoc-lib-info
       (bb/install)))
 
 (defn deploy "Deploy the JAR to Clojars." [opts]
   (-> opts
-      (assoc :lib lib :version version)
+      assoc-lib-info
       (bb/deploy)))
+
+(defn uber [opts]
+  (-> opts
+      assoc-lib-info
+      (bb/clean)
+      (bb/uber)))
